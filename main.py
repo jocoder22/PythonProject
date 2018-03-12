2ae642b71b28181c866fb17d37db2565cfbfb8fd
Okigbo <okigbookey@gmail.com>
2018-03-12 09:00:52 -0400

    Feat: Pythonic reverse function

---
+++ main.py
@@ -0,0 +1,11 @@
+# reverse
+xlist = [5,6,7, "man", 8,9,"girl"]
+xtuple = (7,4,"gon","dog")
+x = "Woman"
+
+def reverse1(parameter):
+    return parameter[::-1]
+
+# for list
+def reverse2(parameter):
+    return parameter.reverse()
