9524673ba13f982fde996f748fbc6c6cd7ff7ba1
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-03-12 18:18:32 -0400

    feat: Add one-line reversed code

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python
+
 # reverse
-xlist = [5,6,7, "man", 8,9,"girl"]
+xlist = [5,6,7, "man", 8, 9,"girl"]
 xtuple = (7,4,"gon","dog")
 x = "Woman"
 
@@ -9,3 +11,14 @@ def reverse1(parameter):
 # for list
 def reverse2(parameter_list):
     return parameter.reverse()
+
+data = xlist
+xl_reversed =  list(data[i] for i in range(len(data)-1, -1, -1))
+data = xtuple
+xt_reversed = tuple(data[i] for i in range(len(data)-1, -1, -1))
+data = x
+x_reversed = "".join(data[i] for i in range(len(data)-1, -1, -1))
+
+print(xl_reversed)
+print(xt_reversed)
+print(x_reversed)
