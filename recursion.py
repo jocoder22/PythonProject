3efce0c0c62a61fe5cc510e119c1121ffe43949b
Okigbo <okigbookey@gmail.com>
2018-03-16 13:22:15 -0400

    Chores: Add new file

---
+++ recursion.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import doctest
+'''
+python -i <file_name.py>
+The above will keep your session running
+and will monitor for code changes
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '___main__':
+    doctest.testmod()
