8160b0e01ff00eec4a0f94fa0d61e827899d93d4
Okigbo <okigbookey@gmail.com>
2018-03-16 13:27:37 -0400

    Feat: Add permute function

--- recursion.py
+++ recursion.py
@@ -7,7 +7,19 @@ and will monitor for code changes
 '''
 
 
+def permute(s):
+    out = []
 
+    # Base Case
+    if len(s) == 1:
+        out = [s]
+
+    else:
+        for i, let in enumerate(s):
+            for perm in permute(s[:i] + s[i + 1:]):
+                out += [let+perm]
+    
+    return out
 
 
 
