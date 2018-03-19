423fdca35bb4298a2ee57c8405478d9ba5b692f0
Okigbo <okigbookey@gmail.com>
2018-03-19 10:22:22 -0400

    Feat: Add myTree

---
+++ tree.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import doctest
+'''
+python -i <file_name.py>
+The above will keep your session running
+and will monitor for code changes
+'''
+
+MyTree = ['a', # root                                        [a]
+           ['b',   # left subtree                           /   \
+            ['d', [], []],          #                      /     \
+            ['e', [], []]          #                      /       \
+           ],                       #                   [b]       [c]
+           ['c',   # right subtree                     /   \       |
+            ['f', [], []], #                          /     \      |
+            []                     #                [e]     [d]   [f]
+           ]
+         ]
+
+         
+if __name__ == '__main__':
+    doctest.testmod(verbose=True)
