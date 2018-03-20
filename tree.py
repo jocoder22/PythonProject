f7302c929857c86b6845f8869595d22c0fe4e2d1
Okigbo <okigbookey@gmail.com>
2018-03-20 11:39:15 -0400

    Feat: Add BinHeap class

--- tree.py
+++ tree.py
@@ -176,7 +176,24 @@ r.getRightChild().getRootVal()
 
 
 
+class BinHeap:
 
+    def __init__(self):
+        self.heapList = [0]
+        self.currentSize = 0
+
+    def percUp(self, i):
+        while i // 2 > 0:
+            if self.heapList[i] < self.heapList[i // 2]:
+                tmp = self.heapList[i // 2]
+                self.heapList[i // 2] = self.heapList[i]
+                self.heapList[i] = tmp
+            i = i // 2
+
+    def insert(self, k):
+        self.heapList.append(k)
+        self.currentSize = self.currentSize + 1
+        
 
 
 if __name__ == '__main__':
