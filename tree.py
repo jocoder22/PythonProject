22a594a949667e9033b4553151812da0562ff46d
Okigbo <okigbookey@gmail.com>
2018-03-21 12:03:07 -0400

    Feat: Update TreeNode class

--- tree.py
+++ tree.py
@@ -336,7 +336,26 @@ class BinarySearchTree:
                     self.rightChild.parent = self.parent
 
     def findSuccessor(self):
-        pass
+        succ = None
+        if self.hasRightChild():
+            succ = self.rightChild.findMin()
+        else:
+            if self.parent:
+                if self.isLeftChild():
+                    succ = self.parent
+                else:
+                    self.parent.rightChild = None
+                    succ = self.parent.findSuccessor()
+                    self.parent.rightChild = self
+        return succ
+
+    def findMin(self):
+        current = self
+        while current.hasLeftChild():
+            current = current.leftChild
+        return current
+
+    
             
 
     def __iter__(self):
