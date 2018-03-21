fc99792d32d7dc001302cfcebff8fe65d60624ca
Okigbo <okigbookey@gmail.com>
2018-03-21 11:08:36 -0400

    Feat: Add BinarySearchTree

--- tree.py
+++ tree.py
@@ -230,5 +230,28 @@ class BinHeap:
 
 
 
+##*************************************###
+##  Binary Search Implementation       ###
+##*************************************###
+
+
+class BinarySearchTee:
+
+    def __init__(self):
+        self.root = None
+        self.size = 0
+
+    def length(self):
+        return self.size
+
+    def __len__(self):
+        return self.size
+
+    def __iter__(self):
+        return self.root.__iter__()
+
+        
+
+
 if __name__ == '__main__':
     doctest.testmod(verbose=True)
