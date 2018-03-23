e36e835d562c09fbaa0b63b5666531a4d12045da
Okigbo <okigbookey@gmail.com>
2018-03-23 11:48:06 -0400

    Feat: Update regxx function

--- regularExp.py
+++ regularExp.py
@@ -46,12 +46,12 @@ test_patterns2 = [ '[a-z]',  		# lower case a to z
 
 
 test_text3 = 'This is a string with Numbers 1238, symbols #$hast, 234-real'
-test_patterns3 = [ r'\d+',  	# sequence of digits
-				  r'\D+',		# sequence of non digits
-				  r'\s+',		# sequence of whitespaces
-				  r'\S+',		# sequence of non whitespaces
-				  r'\w+',		# alphanumeric characters
-				  r'\W+',		# non-alphanumeric characters
+test_patterns3 = [ r'\d+',  	# sequence of digits, this is equivalent to the class [0-9].
+				  r'\D+',		# sequence of non digits, this is equivalent to the class [^0-9].
+				  r'\s+',		# sequence of whitespaces, this is equivalent to the class [ \t\n\r\f\v].
+				  r'\S+',		# sequence of non whitespaces, this is equivalent to the class [^ \t\n\r\f\v].
+				  r'\w+',		# alphanumeric characters,  this is equivalent to the class [a-zA-Z0-9_]
+				  r'\W+',		# non-alphanumeric characters,  this is equivalent to the class [^a-zA-Z0-9_]
 				]
 
 mult_find(test_patterns,test_text)
@@ -68,8 +68,8 @@ print(re.findall(pattern_exclusion,text_exclusion))
 def regxx(expr, s):
 	ss = s.split()
 	if re.search(expr, s):
-		print(f'Some {expr} found in {s} :-)')
-		print(re.search(expr,s))
+		print(f'Some {expr} found in -- {s} -- :-)')
+		print(re.findall(expr,s))
 	else:
 		print(f'No {expr} found :-)')
 
