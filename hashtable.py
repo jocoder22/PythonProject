08186157d144c94081b95358db9b119d1c911929
Okigbo <okigbookey@gmail.com>
2018-03-28 15:44:29 -0400

    Add hashfunction

--- hashtable.py
+++ hashtable.py
@@ -7,6 +7,20 @@ and will monitor for code changes
 '''
 
 
+class HashTable(object):
+
+    def __init__(self, size):
+
+        self.size = size
+        self.slots = [None] * self.size
+        self.data = [None] * self.size
+
+    def put(self, key, data):
+        hashvalue = self.hashfunction(key, len(self.slots))
+
+        if self.slots[hashvalue] == None:
+            self.slots[hashvalue] = key
+
 
 
 
