5dd64170790af352c0a015e8ff1cc4086204ca72
Okigbo <okigbookey@gmail.com>
2018-03-30 15:43:27 -0400

    Feat: update HashTable Class

--- hashtable.py
+++ hashtable.py
@@ -45,23 +45,30 @@ class HashTable(object):
     def rehash(self, oldhash, size):
         return (oldhash + 1) % size
 
-        def get(self, key):
-            startslot = self.hashfunction(key, len(self.slots))
-            data = None
-            stop = False
-            found = False
-            position = startslot
+    def get(self, key):
+        startslot = self.hashfunction(key, len(self.slots))
+        data = None
+        stop = False
+        found = False
+        position = startslot
 
-            while self.slots[position] != None and not found:
-                if self.slots[position] == key:
-                    found = True
-                    data = self.data[position]
+        while self.slots[position] != None and not found:
+            if self.slots[position] == key:
+                found = True
+                data = self.data[position]
 
-                else:
-                    position = self.rehash(position, len(self.slots))
-                    if position = startslot:
-                        stop = True
-            return data
+            else:
+                position = self.rehash(position, len(self.slots))
+                if position = startslot:
+                    stop = True
+        return data
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __setitem__(self, key, data):
+        self.put(key, data)
+
 
 
 if __name__ == '__main__':
