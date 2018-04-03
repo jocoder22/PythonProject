445de4974dde9d6f0c38ccbe69d562daf081c7c2
Okigbo <okigbookey@gmail.com>
2018-04-03 14:16:38 -0400

    Feat: Add descending sort method

--- selectionsort.py
+++ selectionsort.py
@@ -8,7 +8,7 @@ and will monitor for code changes
 
 def selection_sort(arr):
     '''
-    This function performs bubble sort method
+    This function performs selection sort method
      
     Input: List
      
@@ -25,6 +25,8 @@ def selection_sort(arr):
     [3, 4, 14, 15, 18, 20, 23, 50]
     '''
     for slot in range(len(arr)-1,0,-1):
+        # the length is reversed
+        # looking for the maximium
         maxp = 0
         for location in range(1,slot+1):
             if arr[location] > arr[maxp]:
@@ -32,6 +34,33 @@ def selection_sort(arr):
         
         arr[slot], arr[maxp] = arr[maxp], arr[slot]
 
+def selection_sort(arr):
+    '''
+    This function performs selection sort descending method
+     
+    Input: List
+     
+    Output: List
+     
+    Test:
+    >>> arr = [5,3,8,4,5,90,23,14,2]
+    >>> arr2 = [15,3,18,4,50,23,14,20]
+    >>> selection_sort_desc(arr2)
+    >>> selection_sort_desc(arr)
+    >>> arr
+    [90, 23, 14, 8, 5, 5, 4, 3, 2]
+    >>> arr2
+    [50, 23, 20, 18, 15, 14, 4, 3]
+    '''
+    for slot in range(len(arr)-1,0,-1):
+        # the length is reversed
+        # looking for the maximium
+        maxp = 0
+        for location in range(1,slot+1):
+            if arr[location] < arr[maxp]:
+                maxp = location
+        
+        arr[slot], arr[maxp] = arr[maxp], arr[slot]
 
 
 if __name__ == '__main__':
