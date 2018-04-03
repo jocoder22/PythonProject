3151bc7bd85d52e035a2d3f1a30e1962668e0e47
Okigbo <okigbookey@gmail.com>
2018-04-03 12:46:09 -0400

    Feat: add selection_sort function

---
+++ selectionsort.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import doctest
+'''
+python -i <file_name.py>
+The above will keep your session running
+and will monitor for code changes
+'''
+
+def selection_sort(arr):
+    '''
+    This function performs bubble sort method
+     
+    Input: List
+     
+    Output: List
+     
+    Test:
+    >>> arr = [5,3,8,4,5,90,23,14,2]
+    >>> arr2 = [15,3,18,4,50,23,14,20]
+    >>> selection_sort(arr2)
+    >>> selection_sort(arr)
+    >>> arr
+    [2, 3, 4, 5, 5, 8, 14, 23, 90]
+    >>> arr2
+    [3, 4, 14, 15, 18, 20, 23, 50]
+    '''
+    for slot in range(len(arr)-1,0,-1):
+        maxp = 0
+        for location in range(1,slot+1):
+            if arr[location] > arr[maxp]:
+                maxp = location
+        
+        arr[slot], arr[maxp] = arr[maxp], arr[slot]
+
+
+
+if __name__ == '__main__':
+    doctest.testmod(verbose=True)
