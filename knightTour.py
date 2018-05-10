5c04d6306c8a0ccc4633d9a06432acb26cda049e
Okigbo <okigbookey@gmail.com>
2018-05-10 08:06:15 -0400

    Feat: Add function KnightGraph and posToNodeId

---
+++ knightTour.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+
+def KnightGraph(bdsize):
+    ktGraph = Graph()
+    for row in range(bdsize):
+        for col in range(bdsize):
+            nodeId = postToNodeId(row, col, bdsize)
+            newPositions = genLegalMoves(row, col, bdsize)
+            for e in newPositions:
+                nid = postToNodeId(e[0], e[1], bdsize)
+                ktGraph.addEdge(nodeId, nid)
+    return ktGraph
+
+
+def postToNodeId(row, column, board_size):
+    return (row * board_size) + column
+
