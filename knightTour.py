7b165e33175ebe1d8c0b37eebae6eced68b13486
Okigbo <okigbookey@gmail.com>
2018-05-10 08:17:01 -0400

    Feat: add function genLegalMoves

--- knightTour.py
+++ knightTour.py
@@ -17,3 +17,15 @@ def KnightGraph(bdsize):
 def postToNodeId(row, column, board_size):
     return (row * board_size) + column
 
+
+def genLegalMoves(x, y, bdsize):
+    newMoves = []
+    moveOffsets = [(-1, -2), (-1, 2), (-2, -1), (-2, 1),
+                   (1, -2), (1, 2), (2, -1), (2, 1)]
+    for i in moveOffsets:
+        newX = x + i[0]
+        newY = y + i[1]
+        if legalCoord(newX, bdsize) and legalCoord(newY, bdsize):
+            newMoves.append((newX, newY))
+    return newMoves
+    
