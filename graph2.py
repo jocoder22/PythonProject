b7a89bfecfb6d93f1b32e52b19d719b31183280c
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-05-12 13:17:39 -0400

    Update graph2.py

--- graph2.py
+++ graph2.py
@@ -15,7 +15,7 @@ class Node:
     def __init__(self, num):
         self.num = num
         self.visit_state = State.unvisited
-        self.adjacent = OrderedDict()
+        self.adjacent = OrderedDict()  # key = Node, value = Weight
 
     def __str__(self):
         return str(self.num)
