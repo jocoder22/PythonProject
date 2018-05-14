0218135712e15c93ce980b2c7713b62122a01268
Okigbo <okigbookey@gmail.com>
2018-05-14 14:04:59 -0400

    Feat: mymargin function

--- problem1.py
+++ problem1.py
@@ -7,5 +7,20 @@ def myprofit(prices_list):
         minprice = min(minprice, price)
         profit = price - minprice
         maxprofit = max(maxprofit, profit)
-    
+
     return maxprofit
+
+
+def mymargin(prices_day):
+
+    max_margin = 0
+
+    for i in range(len(prices_day) - 1):
+        price_diff = abs(prices_day[i] - prices_day[i+1])
+        max_margin = max(max_margin, price_diff)
+
+    return max_margin
+
+
+myprofit([10, 23, 22, 56, 21, 11, 19, 45])
+myprofit([7, 7, 9, 12, 20, 23, 34, 35, 46, 41, 39, 33, 31, 29, 42])
