52e43bbe7476245078fac79f94d0b10bd4751965
Okigbo <okigbookey@gmail.com>
2018-05-16 08:48:24 -0400

    Feat: Add fib, fibR function

---
+++ problem3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+
+def fib(n):
+    a, b = 1, 1
+    for i in range(n-1):
+        a, b = b, a+b
+    return a
+
+
+def fibR(n):
+    if n == 1 or n == 2:
+        return 1
+    return fibR(n-1) + fibR(n-2)
+
+
