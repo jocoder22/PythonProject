f3ac8e3ed3eed3cc597c0e1ba4bb840212874026
Okigbo <okigbookey@gmail.com>
2018-05-16 11:36:46 -0400

    Feat: add memoization class and decorator

--- problem3.py
+++ problem3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 
+# Using looping technique
 def fib(n):
     a, b = 1, 1
     for i in range(n-1):
@@ -8,6 +9,7 @@ def fib(n):
     return a
 
 
+# Using recursion
 def fibR(n):
     if n == 1 or n == 2:
         return 1
@@ -17,8 +19,43 @@ def fibR(n):
 a, b = 0, 1
 
 
+# Using generators
 def fibI():
     global a, b
     while True:
         a, b = b, a+b
         yield a
+
+
+# Using memoization
+def fibmemo(fn, arg):
+    memo = {}
+    if arg not in memo:
+        memo[arg] = fn(arg)
+    return memo[arg]
+
+
+fibm = fibmemo(fib, 7)
+print(fibm)
+
+
+# Using memoization as decorator
+class Memoize:
+    def __init__(self, fn):
+        self.fn = fn
+        self.memo = {}
+
+    def __call__(self, arg):
+        if arg not in self.memo:
+            self.memo[arg] = self.fn(arg)
+            return self.memo[arg]
+
+
+@Memoize
+def fib2(n):
+    a, b = 1, 1
+    for i in range(n-1):
+        a, b = b, a+b
+    return a
+
+
