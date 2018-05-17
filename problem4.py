e5c541a760f686dc413961a13992275c3fe83d00
Okigbo <okigbookey@gmail.com>
2018-05-17 15:13:56 -0400

    Feat: Add dice7 function

--- problem4.py
+++ problem4.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python
 
+from random import randint
+
+
+def dice7():
+    return randint(1, 7)
+
+
+def convert7to5():
+    roll = 7
+
+    while roll > 5:
+        roll = dice7()
+        print('dice7() produced a roll of', roll)
+    print(' Your final returned roll is below:')
+    return roll
+
