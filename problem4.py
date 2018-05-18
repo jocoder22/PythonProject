e4f30507f17108aebf1e14dd4dc0f3803e0c1092
Okigbo <okigbookey@gmail.com>
2018-05-18 07:43:50 -0400

    Feat: Update

--- problem4.py
+++ problem4.py
@@ -13,6 +13,25 @@ def convert7to5():
     while roll > 5:
         roll = dice7()
         print('dice7() produced a roll of', roll)
-    print(' Your final returned roll is below:')
+    print('    Your final returned roll is below:')
     return roll
 
+
+def dice5():
+    randint(1, 5)
+
+
+def convert5to7():
+    while True:
+        roll_1 = dice5()
+        roll_2 = dice5()
+
+        num = ((roll_1 - 1) * 5)  + roll_2
+
+        if num > 21:
+            continue
+
+        return num %7 + 1
+
+        
+convert7to5()
