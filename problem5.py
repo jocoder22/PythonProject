3453cae7adaf1746d656fecc4e76e9dc5f44d4fa
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-05-19 22:49:53 -0400

    Update: file

--- problem5.py
+++ problem5.py
@@ -18,7 +18,7 @@ def squareRoot(n):
     return k
 
 
-def better_sqaure(n):
+def better_square(n):
     if n < 0:
         return ValueError
 
@@ -29,18 +29,18 @@ def better_sqaure(n):
     high = (n // 2) + 1
 
     while low + 1 < high:
-        mid = low + (high + low)//2
+        mid = low + (high - low)//2
 
         square = mid ** 2
 
         if square == n:
             return mid
         elif square < n:
-            low =mid
+            low = mid
         else:
             high = mid
     return low
 
 
-squareRoot(89)
-better_sqaure(56)
+# squareRoot(89)
+# better_square(56)
