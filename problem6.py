4b2619ac28a7eacf31430a7041607c8250183b33
Okigbo <okigbookey@gmail.com>
2018-05-22 11:57:37 -0400

    Feat: Add funtion threeM

---
+++ problem6.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+
+def threeM(lst):
+    high = max(lst[0], lst[1])
+    low = min(lst[0], lst[1])
+
+    high_prod2 = lst[0]*lst[1]
+    low_prod2 = lst[0]*lst[0]
+
+    high_prod3 = lst[0]*lst[1]*lst[2]
+
+    for num in lst[2:]:
+        high_prod3 = max(high_prod3, num*high_prod2, num*low_prod2)
+
+        high_prod2 = max(high_prod2, num*high, num*low)
+        low_prod2 = max(low_prod2, num*high, num*low)
+
+        high = max(high, num)
+        low = min(low, num)
+
+    return high_prod3
