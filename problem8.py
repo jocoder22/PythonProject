3420bf5939bb3af45e46a480674ba56e663d0939
Okigbo <okigbookey@gmail.com>
2018-05-24 13:27:43 -0400

    problem8.py: add isBST2 function

--- problem8.py
+++ problem8.py
@@ -21,3 +21,18 @@ def isBST(tree, minval=NEG_INF, maxval=INF):
 
     return isBST(tree.left, minval, tree.val) and isBST(tree.right,
                                                         tree.val, maxval)
+
+
+def isBST2(tree, lastNode=[NEG_INF]):
+    if tree is None:
+        return True
+
+    if not isBST2(tree.left, lastNode):
+        return False
+
+    if tree.val < lastNode[0]:
+        return False
+
+    lastNode[0] = tree.val
+
+    return isBST2(tree.right, lastNode)
