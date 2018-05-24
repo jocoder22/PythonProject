a77a0b8e0a97e51c753ee37381379794defecad5
Okigbo <okigbookey@gmail.com>
2018-05-24 13:18:31 -0400

    problem8.py: add isBST function

--- problem8.py
+++ problem8.py
@@ -10,3 +10,14 @@ class Node:
 
 INF = float("infinity")
 NEG_INF = float("-infinity")
+
+
+def isBST(tree, minval=NEG_INF, maxval=INF):
+    if tree is None:
+        return True
+
+    if not minval <= tree.val <= maxval:
+        return False
+
+    return isBST(tree.left, minval, tree.val) and isBST(tree.right,
+                                                        tree.val, maxval)
