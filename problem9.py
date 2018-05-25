3682e6e22b29b6d0f1cc8d228c5b0583a94f6179
Okigbo <okigbookey@gmail.com>
2018-05-25 13:44:01 -0400

    Feat: add removeDup function

---
+++ problem9.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+
+def removeDup(lst):
+    result = []
+
+    seen = set()
+
+    for char in lst:
+        if char not in seen:
+            seen.add(char)
+            result.append(char)
+
+    return ''.join(result)
+
+    
