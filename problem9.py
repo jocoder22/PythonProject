01be391ff3a0ed9ec260bcc42e81bda2af1cf705
Okigbo <okigbookey@gmail.com>
2018-05-25 13:58:27 -0400

    Feat problem9.py: add and update function

--- problem9.py
+++ problem9.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python
 
 
-def removeDup(lst):
+def removeDupString(s):
+    result = []
+
+    seen = set()
+
+    for char in s:
+        if char not in seen:
+            seen.add(char)
+            result.append(char)
+
+    return ''.join(result)
+
+
+print(removeDupString('babababadadamamayayameme'))
+
+
+def removeDupList(lst):
     result = []
 
     seen = set()
@@ -11,7 +27,10 @@ def removeDup(lst):
             seen.add(char)
             result.append(char)
 
-    return ''.join(result)
+    return result
 
 
-print(removeDup('babababadadamamayayameme'))
+print(removeDupList([9, 18, 29, 18, 9, 10, 10]))
+print(removeDupList(['lit', 2, 4, 2, 3, 3, 3, 'lit', 'ma', 'lit', 'ma']))
+
+
