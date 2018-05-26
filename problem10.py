42c10d3fe66bfe7a59009f867d9025c34418446d
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-05-26 19:56:00 -0400

    problem10.py: add sumUP function

--- problem10.py
+++ problem10.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python
+
+
+def sumUP(lst, target):
+    seen = set()
+
+    for i in lst:
+        num2 = target - i
+
+        if num2 in seen:
+            return True
+
+        seen.add(i)
+    
+    return False
