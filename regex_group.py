24ea235f8d9e3d051ef4b1044b761fa5c0996bc2
Okigbo <okigbookey@gmail.com>
2018-05-30 15:36:03 -0400

    regex_group.py: Update

--- regex_group.py
+++ regex_group.py
@@ -7,3 +7,9 @@ text = 'Babamiloo'
 pattern = r'(ba)'
 mysearch = re.compile(pattern)
 print(mysearch.match(text))
+
+text2 = 'My son in higher education lives with our cat catty'
+pattern2 = r'(cat)\w+'
+mysearch2 = re.compile(pattern2)
+result = mysearch2.findall(text2)
+
