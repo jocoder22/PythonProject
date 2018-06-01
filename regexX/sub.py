1ec3055e4d1299656a8cfd1716fe9286f5d67ed5
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-06-01 03:47:50 -0400

    Update

--- regexX/sub.py
+++ regexX/sub.py
@@ -18,3 +18,23 @@ text2 = "My telephone number is 222-222-2222"
 phonePattern = r'22'
 newNumber = re.sub(phonePattern, "55", text2)
 print(newNumber)
+
+
+# repl can be a function
+# the pattern must be a group
+def repl_function(matchobj):
+    if matchobj.group(1) in r'[0,1,2,3,4]':
+        return '9'
+    else:
+        return 's'
+
+text2a = 'A good day 32'
+newAddress =  re.sub('([a-zA-Z0-9])', repl_function, text2a)
+print(newAddress)
+
+
+def repl_function2(matchobj):
+    if matchobj.group(1) == 'Boy': return "John"
+    else: return matchobj(1)
+
+print(re.sub(r'(Boy)', repl_function2, 'Boy is a man'))
