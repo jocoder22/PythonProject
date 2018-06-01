2690108a1e81a4e834331b0b4a3a4fc053cf0447
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-06-01 01:37:43 -0400

    update  Changes to be committed: 	modified:   regexX/split.py

--- regexX/split.py
+++ regexX/split.py
@@ -18,7 +18,10 @@ len(split_result)
 # if maxsplit > maximum possible splits, returns all possible splits
 result_maxsplit = result.split(text, 3)
 
+pat = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
-
+# word count
+word_list = result.split(pat)
+len(word_list)
 
 
