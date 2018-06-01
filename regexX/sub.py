f70a5646805266bb02f08e9be42dce678533b484
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-06-01 02:23:02 -0400

    update  Changes to be committed:  	modified:   sub.py

--- regexX/sub.py
+++ regexX/sub.py
@@ -4,6 +4,7 @@ import re
 
 # sub(repl, string, count=0) --> with compilation
 # sub(pattern, repl, string, count=0) --> without compilation
+# re.sub returns the modified string
 
 text = 'Mary birthday is on 20th of May'
 pattern = r'[0-9]+'
@@ -11,3 +12,9 @@ replace = re.compile(pattern)
 
 newBirthday = replace.sub("23",text)
 print(newBirthday)
+
+# re.sub will replace the leftmost non-overlapping match
+text2 = "My telephone number is 222-222-2222"
+phonePattern = r'22'
+newNumber = re.sub(phonePattern, "55", text2)
+print(newNumber)
