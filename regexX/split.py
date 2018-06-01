5b2b9a1496efb254a8cebda967bd0dc12ff55ae3
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-06-01 00:24:13 -0400

    update

--- regexX/split.py
+++ regexX/split.py
@@ -2,12 +2,23 @@
 
 import re
 
-# split(string, maxsplit=0)
+# split(string, maxsplit=0) --> returns a list
 
-text = 'This is bright sunny day'
+
+text = 'This is a bright sunny day'
 pattern = r'\W'
 result = re.compile(pattern)
 split_result = result.split(text)
+
+# find number of element in the list
+len(split_result)
+
+# maxsplit -- maximum number of split you want
+# if less than maximum possible splits, returns the remainder
+# if maxsplit > maximum possible splits, returns all possible splits
+result_maxsplit = result.split(text, 3)
+
+
 
 
 
