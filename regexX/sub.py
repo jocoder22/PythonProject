f375a76d9c52b984635d035de368026efb739f20
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-06-01 02:14:33 -0400

    Update file  Changes to be committed: 	modified:   sub.py

--- regexX/sub.py
+++ regexX/sub.py
@@ -2,4 +2,12 @@
 
 import re
 
+# sub(repl, string, count=0) --> with compilation
+# sub(pattern, repl, string, count=0) --> without compilation
 
+text = 'Mary birthday is on 20th of May'
+pattern = r'[0-9]+'
+replace = re.compile(pattern)
+
+newBirthday = replace.sub("23",text)
+print(newBirthday)
