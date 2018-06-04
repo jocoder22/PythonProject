6f964accb1ee80d194a1def22f97499e0215ea03
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-06-03 23:33:55 -0400

    Updates  Changes to be committed: 	modified:   regexX/sub.py

--- regexX/sub.py
+++ regexX/sub.py
@@ -42,5 +42,6 @@ print(re.sub(r'(Boy)', repl_function2, 'Boy is a man')) # John is a man
 
 # using subn
 textn = "This is the 15th of March, 1990"
-subnPattern = r'(\d+\w+\s)'
-subnn = re.subn(subnPattern, 'nodigit',textn, 2, re.I)
+subnPattern = re.compile(r'(\d+\w+)')
+subnn = subnPattern.subn('nodigit', textn, 2, re.I)
+
