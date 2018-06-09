fbbfd6ecb038dce24b5750cdbde2407d8d7ecf1d
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-06-08 23:42:11 -0400

    Update  Changes to be committed: 	modified:   group.py

--- regexX/group.py
+++ regexX/group.py
@@ -17,7 +17,7 @@ zipcode = r'((?P<code1>\d{5})-(?(code1)(\d{2})|(\w{2})))'
 correctzip = re.search(zipcode, '12222-6879')
 
 
-extension = r'((?P<area>\d{3})-(?P=area)-\d{4} (?P<ex>Ext-)(?(ex)\d{4}))'
+extension = r'((?P<area>\d{3})-(?P=area)-\d{4}(?P<ex> Ext-)?(?(ex)\d{4}))'
 check = re.compile(extension)
 correctext = check.search('121-456-873 Ext-5698')
 
