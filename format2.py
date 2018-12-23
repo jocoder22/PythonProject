a72a923806fb52dd92c798f873aebba718475290
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-12-23 07:41:52 -0500

    Update

--- format2.py
+++ format2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import timeit
+
 for i in range(40):
     print(i, end='')
     
@@ -25,15 +27,15 @@ def shuffle_word(s):
     import random
     mylist = list()
     st = [i for i in s]
-    for _ in range(5000):
+    for _ in range(1000):
        
         random.shuffle(st)
         new = "".join(st)
 
         if new not in mylist:
             mylist.append(new)
-    print(mylist)
-    print(len(mylist))
+    # print(mylist)
+    # print(len(mylist))
     return mylist
 
 def wordPermut(w):
@@ -42,13 +44,29 @@ def wordPermut(w):
     permutSet = permutations(w)
 
     for i in list(permutSet):
-        if i not in mylist2:
-            mylist2.append(''.join(i))
-    print(mylist2)
-    print(len(mylist2))
+        mylist2.append(''.join(i))
+    # print(mylist2)
+    # print(len(mylist2))
     return mylist2
 
 
-shuffle_word("tlcccr")
+# shuffle_word("tlcccr")
+myword = "tlcccr"
 wordPermut("tlcccr")
 word_form("dkeke", "loos")
+shuffle_word("tlcccr")
+
+ti = timeit.timeit('wordPermut(myword)', number=10, globals=globals())
+t2 = timeit.timeit('shuffle_word(myword)', number=10, globals=globals())
+
+def program_performance(a , b , c):
+    import timeit
+    t11 = timeit.timeit('a(c)', number=100, globals=globals())
+    t22 = timeit.timeit('b(c)', number=100, globals=globals())
+    if t11 > t22: 
+        r = round(t11/t22, 1)
+        print('{} is {} times faster then {}'.format(a, r, b))
+    else: 
+        r = round(t22/t11, 1)
+        print('{} is {} times faster then {}'.format(b, r, a))
+
