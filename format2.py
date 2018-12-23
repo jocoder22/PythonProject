e9a2acd1c2f9ed9c7247a4006eef133f137f4182
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-12-23 07:05:52 -0500

    Update

--- format2.py
+++ format2.py
@@ -8,3 +8,16 @@ print()
 # formating using keyword argument
 print('This is {x} {} in {}'.format('morning', 'USA', x='Christmas'))
 print('This is {x} {1} in {0}'.format('morning', 'USA', x='Christmas'))
+
+# Type formatting
+s1 = 'This is {} {}'.format('String', 'One')
+print(s1)
+print([i for i in range(10)])
+
+def word_form(*args):
+    for k in args:
+        for i in k:
+           print(i, end='   ')
+        print('')
+
+word_form("dkeke", "loos")
