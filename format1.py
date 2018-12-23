776fe65c586e7aab70abe15606de50bdef6e2fcc
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-12-22 20:35:43 -0500

    Update

--- format1.py
+++ format1.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python
 
+city = ["New York", "Newark", "Chicago", "Maima", "Baltimore",
+        "New Park", "Texas New City", "Kens Dews", "Portland"]
+
+for index, value in enumerate(city):
+    print("Location: {:30}  Miles: {}000".format(value, index))
+    print("Location: %-*s Miles: %s000" %(20, value, index))
+    print("Location: {}".format(value).ljust(40) + "  Miles: {}0000".format(index))
