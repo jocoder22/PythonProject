ca89ae3336a96b14fcb49fddef7a5667b7d82dcc
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-12-23 08:06:52 -0500

    Update

--- format2.py
+++ format2.py
@@ -56,17 +56,20 @@ wordPermut("tlcccr")
 word_form("dkeke", "loos")
 shuffle_word("tlcccr")
 
-ti = timeit.timeit('wordPermut(myword)', number=10, globals=globals())
-t2 = timeit.timeit('shuffle_word(myword)', number=10, globals=globals())
+# ti = timeit.timeit('wordPermut(myword)', number=10, globals=globals())
+# t2 = timeit.timeit('shuffle_word(myword)', number=10, globals=globals())
 
-def program_performance(a , b , c):
+def program_performance(a, b, c):
     import timeit
-    t11 = timeit.timeit('a(c)', number=100, globals=globals())
-    t22 = timeit.timeit('b(c)', number=100, globals=globals())
+    f1 = a.__name__
+    f2 = b.__name__
+    t11 = timeit.timeit('a(c)', number=100, globals=locals())
+    t22 = timeit.timeit('b(c)', number=100, globals=locals())
     if t11 > t22: 
         r = round(t11/t22, 1)
-        print('{} is {} times faster then {}'.format(a, r, b))
+        print('{} is {} times faster then {}'.format(f2, r, f1))
     else: 
         r = round(t22/t11, 1)
-        print('{} is {} times faster then {}'.format(b, r, a))
+        print('{} is {} times faster then {}'.format(f1, r, f2))
 
+program_performance(shuffle_word , wordPermut, "boutte")
