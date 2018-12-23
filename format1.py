1c66f6c4135687f2160a7a25dd84e50eeb625f6c
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-12-22 21:19:30 -0500

    Update

--- format1.py
+++ format1.py
@@ -4,7 +4,7 @@ city = ["New York", "Newark", "Chicago", "Maima", "Baltimore",
         "New Park", "Texas New City", "Kens Dews", "Portland"]
 
 for index, value in enumerate(city):
-    print("Location: {0:30}  Miles: {1:6}000 {2:20}".format(value, index+1, "Example I"))
+    print("Location: {0:30}  Miles: {1:6}000 {2:14}".format(value, index+1, "Example I"))
     print("Location: %-*s Miles: %s000  %s" %(20, value, index, "Example II"))
     print("Location: {}".format(value).ljust(40) + "  Miles: {}0000".format(index))
     print("Location: {}\t Miles: {}000".format(value, index).expandtabs(30))
