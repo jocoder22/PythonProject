e253a81571bd4ef7d18f38bb0299d5adfd21d3e8
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-01-03 22:01:25 -0500

    update

--- format/format3.py
+++ format/format3.py
@@ -7,9 +7,19 @@ price  = 940
 visitors = {'name': ['Jone', 'Emmy', 'lorrie', 'kenn', 'jude'],
             'gift': ['shoes', 'bags', 'pens', 'clock', 'printer']}
 
+electronics = {'Microwave': 230,
+              'Cellphone': 780,
+               'Printer': 150,
+               'Radio': 89}
+
 print(f'The book was sold at ${price}')
 
 for keys, values in visitors.items():
     print(f'{keys}: {values}')
     for i in values:
-        print(f'{keys.upper()}: {i}')
+        print(f'{keys.upper()}: {i.capitalize()}')
+
+
+for item, price in electronics.items():
+    tax = 0.193
+    print(f'The price of {item} is ${float(price + (tax * price)):.2f}')
